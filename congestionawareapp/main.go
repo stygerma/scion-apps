dd32aba81efee51de81f695a792e1cb70d9e835d
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-05-15 18:06:30 +0200

    PoC finds paths from scmps.
    It selects the path with least scmp messages.
    Needs filtering in the SCMP handler to filter only congestion messages.
    Needs to put more load (BW) in the messages.

--- congestionawareapp/main.go
+++ congestionawareapp/main.go
@@ -20,7 +20,6 @@ import (
 
 const (
 	localAddrStr = "1-ff00:0:110,127.0.0.1:12345"
-	// sciondLocation = "127.0.0.20:30255"
 )
 
 func main() {
@@ -70,7 +69,7 @@ type client struct {
 	serverAddr *snet.UDPAddr
 	clientAddr *net.UDPAddr
 	paths      PathMap
-	scmpConn   *snet.Conn
+	conn       *snet.Conn
 }
 
 func (c *client) run(remote string) error {
@@ -80,7 +79,7 @@ func (c *client) run(remote string) error {
 	}
 	defer c.shutdown()
 
-	err = c.loopSendMessages(c.scmpConn)
+	err = c.loopSendMessages()
 	fmt.Println("done.")
 	return err
 }
@@ -109,30 +108,30 @@ func (c *client) init(remote string) error {
 			SCMPHandler: c,
 		},
 	)
-	c.scmpConn, err = network.Dial(context.TODO(), "udp", c.clientAddr, c.serverAddr, addr.SvcNone)
+	c.conn, err = network.Dial(context.TODO(), "udp", c.clientAddr, c.serverAddr, addr.SvcNone)
 	if err != nil {
 		return err
 	}
 	go func() {
 		buff := make([]byte, 16384)
 		for {
-			n, from, err := c.scmpConn.ReadFrom(buff)
+			n, from, err := c.conn.ReadFrom(buff)
 			if err != nil {
 				panic(err)
 			}
-			fmt.Printf("scmpConn.Read = %d . From: %v\n", n, from)
+			fmt.Printf("conn.Read = %d . From: %v\n", n, from)
 		}
 	}()
 	return nil
 }
 
 func (c *client) shutdown() {
-	if err := c.scmpConn.Close(); err != nil {
+	if err := c.conn.Close(); err != nil {
 		fmt.Printf("while shutting down: %v\n", err)
 	}
 }
 
-func (c *client) loopSendMessages(conn *snet.Conn) error {
+func (c *client) loopSendMessages() error {
 	// every 1 second send a message, for a total of numMsg messages
 
 	const numMsg = 1000
@@ -141,12 +140,8 @@ func (c *client) loopSendMessages(conn *snet.Conn) error {
 			time.Sleep(time.Second)
 		}
 		p := c.selectBestPath()
-		// appnet.SetPath(c.serverAddr, p)
-		c.serverAddr.Path = p.Path()
-		c.serverAddr.NextHop = p.OverlayNextHop()
 		message := []byte(fmt.Sprintf("this message (%d) sent using path %v", i, p))
-		_, err := conn.WriteTo(message, c.serverAddr)
-
+		err := c.SendMessage(p, message)
 		if err != nil {
 			return err
 		}
@@ -154,21 +149,28 @@ func (c *client) loopSendMessages(conn *snet.Conn) error {
 	return nil
 }
 
-func (c *client) selectBestPath() snet.Path {
-	// idx := rand.Intn(len(c.paths))
-	// return c.paths[idx]
-	// for _, v := range c.paths.m {
-	// 	return v[0]
-	// }
-	idx := rand.Intn(c.paths.Len())
-	i := 0
-	for _, p := range c.paths.m {
-		if i == idx {
-			return p
-		}
-		i++
+func (c *client) SendMessage(p snet.Path, msg []byte) error {
+	appnet.SetPath(c.serverAddr, p)
+	pData := c.paths.Find(PathKey(p))
+	if pData == nil {
+		return fmt.Errorf("path not found, but in use! %v", p)
 	}
-	panic("no paths")
+	pData.sentPkts++
+	_, err := c.conn.WriteTo(msg, c.serverAddr)
+	return err
+}
+
+func (c *client) selectBestPath() snet.Path {
+	var bestPath *PathData
+	var bestScore uint64
+	for _, p := range c.paths.m {
+		score := p.badPkts / (p.sentPkts + 1)
+		if score <= bestScore {
+			bestScore = score
+			bestPath = p
+		}
+	}
+	return bestPath.Path
 }
 
 func check(err error) {
@@ -180,45 +182,58 @@ func check(err error) {
 var _ snet.SCMPHandler = (*client)(nil)
 
 func (c *client) Handle(pkt *snet.Packet) error {
-	// fmt.Printf("SCMP handle. pkt = %v\n", pkt)
-	p := c.paths.Find(*pkt.PacketInfo.Path)
+	p := c.paths.FindFromSpath(pkt.PacketInfo.Path)
 	fmt.Printf("Got SCMP. Path=%v\n", p)
+	if p == nil {
+		panic("logic error: path not found, but in use!")
+	}
+	p.badPkts++
 	hdr, ok := pkt.L4Header.(*scmp.Hdr)
 	fmt.Printf("scmp handler. hdr = %v\nis scmp header? %v\n", hdr, ok)
 
 	return nil
 }
 
-// PathMap stores the keys as a CRC of the reversed paths.
+// PathMap stores the keys as strings of the reversed paths.
 type PathMap struct {
-	m map[string]snet.Path
+	m map[string]*PathData
+}
+
+type PathData struct {
+	Path     snet.Path
+	sentPkts uint64
+	badPkts  uint64
 }
 
 func newPathMap(paths []snet.Path) *PathMap {
 	m := &PathMap{
-		// m: make(map[uint32][]snet.Path),
-		m: make(map[string]snet.Path),
+		m: make(map[string]*PathData),
 	}
+	var alreadyThere bool
 	for _, p := range paths {
-		spath := p.Path().Copy()
-		if err := spath.Reverse(); err != nil {
-			panic(err)
+		key := PathKey(p)
+		_, alreadyThere = m.m[key]
+		if alreadyThere {
+			panic("found two paths sharing the same key!")
 		}
-		key := string(spath.Raw)
-		m.m[key] = p
+		m.m[key] = &PathData{Path: p}
 	}
 	return m
 }
 
+// Len returns how many paths this PathMap contains.
 func (m *PathMap) Len() int {
 	return len(m.m)
 }
 
-// Find finds a path.
-func (m *PathMap) Find(p spath.Path) snet.Path {
+// FindFromSpath finds a path data from a path. Returns nil if not found. The path is reversed.
+func (m *PathMap) FindFromSpath(p *spath.Path) *PathData {
 	fmt.Printf("looking for:\n: %s\n", hex.EncodeToString(p.Raw))
+	return m.Find(string(p.Raw))
+}
+
+func (m *PathMap) Find(key string) *PathData {
 	m.debugPrint()
-	key := string(p.Raw)
 	stored, found := m.m[key]
 	if !found {
 		return nil
@@ -231,4 +246,12 @@ func (m *PathMap) debugPrint() {
 	for k := range m.m {
 		fmt.Printf("> %s\n", hex.EncodeToString([]byte(k)))
 	}
+}
+
+func PathKey(p snet.Path) string {
+	spath := p.Path().Copy()
+	if err := spath.Reverse(); err != nil {
+		panic(err)
+	}
+	return string(spath.Raw)
 }
